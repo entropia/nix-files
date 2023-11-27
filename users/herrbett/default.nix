bd1884cd5bbc2d8b73f20d400c899e78b846ad79
Alexander Sieg <alex@xanderio.de>
2023-11-27 20:51:41 +0100

    users: add herrbett

---
+++ users/herrbett/default.nix
@@ -0,0 +1,11 @@
+{
+  config = {
+    users.users.herrbett = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDl45/IoMMB9IR23yaaPlpDFt0TqDKLXfqv4V/BO+1kL"
+      ];
+    };
+  };
+}
