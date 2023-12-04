613eb334010e7137331eef0700dd052b6f377318
Alexander Sieg <alex@xanderio.de>
2023-12-04 21:25:56 +0100

    overlays: esayOverlays is just cursed

--- flake.nix
+++ flake.nix
@@ -12,7 +12,6 @@
     systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
     imports = [
-      inputs.flake-parts.flakeModules.easyOverlay
       ./hosts
       ./modules
       ./pkgs
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,10 @@
-{
-  perSystem = { pkgs, self', ... }: {
-    overlayAttrs.default = self'.packages; 
+{ self, ... }: {
+
+  flake.overlays.default = final: prev: {
+    inherit (self.packages.${final.system}) wg-access-server;
+  };
+
+  perSystem = { pkgs, ... }: {
     packages = {
       wg-access-server = pkgs.callPackage ./wg-access-server { };
     };
