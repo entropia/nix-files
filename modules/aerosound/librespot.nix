6889f764238010dd416998b8bae4d6bb593f5fc8
Alexander Sieg <alex@xanderio.de>
2024-03-09 18:49:07 +0100

    wip

--- modules/aerosound/librespot.nix
+++ modules/aerosound/librespot.nix
@@ -1,20 +1,7 @@
 { pkgs, lib, ... }:
 let
-  package = pkgs.librespot.overrideAttrs (old: rec {
-    version = "0.5.0-dev";
-    src = pkgs.fetchFromGitHub {
-      owner = "librespot-org";
-      repo = "librespot";
-      rev = "5a322e70e15d4e442a2d681609bb38eec8b31dc8";
-      hash = "sha256-NHburKiaWmcW+HhbmIklwGBVu++kjI0nCeli9WXcUHs=";
-    };
-
-    cargoDeps = old.cargoDeps.overrideAttrs (lib.const {
-      name = "${old.pname}-vendor.tar.gz";
-      inherit src;
-      outputHash = "sha256-otFsv0eoAYIqqtydUOx/WDu4+zi23jeRakaFphvyZJM=";
-    });
-
+  package = pkgs.spotifyd.overrideAttrs (old: {
+    patches = (old.patches or [ ]) ++ [ ./spotifyd-dns-sd.patch ];
   });
 in
 {
