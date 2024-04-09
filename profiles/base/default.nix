7ddd3120887d13906e37cd65de3654f1fcde30d0
Alexander Sieg <alex@xanderio.de>
2024-04-09 18:03:30 +0200

    profile/base: subtitute from entropia cachix

--- profiles/base/default.nix
+++ profiles/base/default.nix
@@ -21,7 +21,13 @@
     };
 
     security.sudo.wheelNeedsPassword = false;
-    nix.settings.trusted-users = [ "@wheel" ];
+
+    nix.settings = {
+      trusted-users = [ "@wheel" ];
+      trusted-substituters = [ "https://entropia.cachix.org" ];
+      substituters = [ "https://entropia.cachix.org" ];
+      trusted-public-keys = [ "entropia.cachix.org-1:a3vy2scFVr0sQvtp2CPlOlzUKmPfbvs1/9VFsqqI5Sk=" ];
+    };
 
     # connect to node using local user name
     deployment.targetUser = lib.mkDefault null;
