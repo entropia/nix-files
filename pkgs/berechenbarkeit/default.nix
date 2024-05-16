f969a2c3cfc40872cfaefb78873585d937b37e88
Leona Maroni <dev@leona.is>
2024-05-16 22:48:14 +0200

    update(berechenbarkeit): 703ecb8 -> b482c73

--- pkgs/berechenbarkeit/default.nix
+++ pkgs/berechenbarkeit/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage {
   pname = "berechenbarkeit";
-  version = "0-unstable-2024-05-12";
+  version = "0-unstable-2024-05-16";
 
   src = fetchFromGitHub {
     owner = "entropia";
     repo = "berechenbarkeit";
-    rev = "703ecb85d66a1798c33feebd1d906a18bf1727e4";
-    hash = "sha256-DtXlaNvWIhDrBMVWOmV1eTskJmqYf8wYLdEfxdbD3oc=";
+    rev = "b482c7398fb68f946e4056bb5c4c183d0dcabce7";
+    hash = "sha256-atFIHF0NQqQG+VLdf8ox1vpg8/8RI6ii8UH7M9Rfwg0=";
   };
 
-  cargoHash = "sha256-Zisj2fpebz4CRwpsg/H+8H/s1Q395lOiVKg9fcZVTtw=";
+  cargoHash = "sha256-Z+APSmwFfqQAbo4cUDtdRQhLs0IuwvyX5jUaIOgcScA=";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [
