ff1bcd4eb5d26b5cb721480547fbdad49ab6cd8d
Alexander Sieg <alex@xanderio.de>
2024-06-21 17:31:42 +0200

    kiosk: unpin nixpkgs and switch to sway

--- modules/kiosk/default.nix
+++ modules/kiosk/default.nix
@@ -32,92 +32,58 @@ in
       else
         [ "fbcon=rotate:3" ];
 
-    # UDev rule for rotating the Touch Input too
-    services.udev.extraRules =
-      "ENV{ID_VENDOR_ID}==\"04e7\",ENV{ID_MODEL_ID}==\"0022\",ENV{WL_OUTPUT}=\"HDMI1\",ENV{LIBINPUT_CALIBRATION_MATRIX}=\"${if cfg.flip180 then "0 1 0 -1 0 1" else "0 -1 1 1 0 0"}\"\n";
-
-    nixpkgs.config.packageOverrides = pkgs: {
-      cage = pkgs.cage.override { xwayland = null; };
-      firefox = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-        extraPolicies = {
-          CaptivePortal = false;
-          DisableFirefoxStudies = true;
-          DisablePocket = true;
-          DisableTelemetry = true;
-          DisableFirefoxAccounts = true;
-          DisableDeveloperTools = true;
-          OfferToSaveLogins = false;
-          FirefoxHome = {
-            Pocket = false;
-            Snippets = false;
-          };
-          UserMessaging = {
-            ExtensionRecommendations = false;
-            SkipOnboarding = true;
-          };
-        };
-
-        extraPrefs = ''
-          // "disable" the pinch to zoom by require a gesture that is large then the screen
-          pref("browser.gesture.pinch.threshold", 5000);
-
-          // disable the "insecure password" warning when using HTTP
-          pref("security.insecure_password.ui.enabled", false);
-        '';
-      };
-    };
-
-    systemd.services."serial-getty@ttyS0".enable = false;
-    systemd.services."serial-getty@hvc0".enable = false;
-    systemd.services."getty@tty1".enable = false;
-    systemd.services."autovt@".enable = false;
-
-    users.users.cage = {
-      isNormalUser = true;
-      uid = 1000;
-      group = "cage";
-    };
-
-    users.groups.cage = { };
+    # WL_OUTPUT must be set otherwise touch input transformation is brocken
+    services.udev.extraRules = ''
+      ENV{ID_INPUT_TOUCHSCREEN}=="1",ENV{WL_OUTPUT}="HDMI1"
+    '';
 
     services.dbus.enable = true;
 
-    services.cage = {
+    environment.variables = {
+      NIXOS_OZONE_WL = 1;
+    };
+
+    programs.chromium = {
       enable = true;
-      user = "cage";
-      program = "${lib.getExe pkgs.firefox} --kiosk ${cfg.url}";
-      extraArguments = [ "-d" ];
-      environment = {
-        MOZ_ENABLE_WAYLAND = "1";
-        LIBSEAT_BACKEND = "logind";
-        # WAYLAND_DEBUG = "1"; # Enable for debugging purposes
+      extraOpts = {
+        "BrowserSignin" = 0;
+        "SyncDisabled" = true;
+        "PasswordManagerEnabled" = false;
+        "SpellcheckEnabled" = false;
       };
     };
 
-    systemd.services."cage-tty1" = {
-      serviceConfig = {
-        Type = "simple";
-        Restart = "on-failure";
-        RestartSec = "2s";
+    programs.sway.enable = true;
+
+    environment.etc."sway/config".text = ''
+      xwayland disable
+
+      input * {
+        map_to_output ${cfg.output}
+      }
+
+      output ${cfg.output} {
+        transform ${if cfg.flip180 then "90" else "270"}
+      }
+
+      exec ${lib.getExe pkgs.chromium} --kiosk ${cfg.url}
+    '';
+
+    # autologin and start sway
+    services.greetd = {
+      enable = true;
+      settings = rec {
+        initial_session = {
+          command = "systemd-cat -t sway sway";
+          user = "kiosk";
+        };
+        default_session = initial_session;
       };
     };
 
-    systemd.services."wlr-randr" = {
-      after = [ "cage-tty1.service" ];
-      partOf = [ "cage-tty1.service" ];
-
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = "${pkgs.wlr-randr}/bin/wlr-randr --output ${cfg.output} --transform ${if cfg.flip180 then "270" else "90"}";
-
-        Restart = "on-failure";
-        RestartSec = "2s";
-      };
-      environment = {
-        XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.cage.uid}/";
-      };
+    users.users.kiosk = {
+      isNormalUser = true;
+      extraGroups = [ "input" ];
     };
-
-    systemd.targets.graphical.wants = [ "wlr-randr.service" ];
   };
 }
