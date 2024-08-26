145021f2de2112bada25adff8ab4f660e5509cbc
Alexander Sieg <alex@xanderio.de>
2024-08-26 20:46:35 +0200

    profiles: only enable backups if secret exists

--- profiles/entropia-cluster-vm/backup.nix
+++ profiles/entropia-cluster-vm/backup.nix
@@ -1,32 +1,49 @@
-{ config, lib, pkgs, ... }: {
-  x.sops.secrets."hosts/${config.networking.hostName}/backup_zweitwohnsitz_password" = {};
- 
-  services.borgbackup.jobs.zweitwohnsitz = {
-    paths = [ "/var/backup" "/var/lib" "/root" ];
-    exclude = [ "'**/.cache'" ];
-    doInit = true;
-    repo =  "ssh://borgbackup@zweitwohnsitz.entropia.de/./${config.networking.hostName}.${config.networking.domain}";
-    encryption = {
-      mode = "repokey-blake2";
-      passCommand = "cat ${config.sops.secrets."hosts/${config.networking.hostName}/backup_zweitwohnsitz_password".path}";
-    };
-    environment = {
-      BORG_RSH = "ssh -i /etc/ssh/ssh_host_ed25519_key";
-      BORG_RELOCATED_REPO_ACCESS_IS_OK = "yes";
-    };
-    preHook = lib.mkIf config.services.postgresql.enable ''
-      ${lib.getExe pkgs.sudo} -u postgres ${pkgs.postgresql}/bin/pg_dumpall --globals-only > /var/backup/postgresql/globals
-      ${lib.getExe pkgs.sudo} -u postgres ${pkgs.postgresql}/bin/psql -t -A -c "SELECT datname FROM pg_database WHERE datname <> ALL ('{template0,template1,postgres}')" | ${pkgs.findutils}/bin/xargs -I DBNAME ${lib.getExe pkgs.sudo} -u postgres ${pkgs.postgresql}/bin/pg_dump -F directory -f /var/backup/postgresql/DBNAME DBNAME
-    '';
-    postCreate = lib.mkIf config.services.postgresql.enable ''
-      rm -r /var/backup/postgresql/*
-    '';
-    readWritePaths = lib.optionals config.services.postgresql.enable [ "/var/backup/postgresql" ];
-    compression = "auto,zlib";
-    startAt = "daily";
-  };
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  config =
+    lib.mkIf (lib.pathIsRegularFile (inputs.self + "/secrets/hosts/${config.networking.hostName}.yaml"))
+      {
+        x.sops.secrets."hosts/${config.networking.hostName}/backup_zweitwohnsitz_password" = { };
 
-  systemd.tmpfiles.rules = lib.mkIf config.services.postgresql.enable [
-    "d /var/backup/postgresql 0700 postgres - - -"
-  ];
+        services.borgbackup.jobs.zweitwohnsitz = {
+          paths = [
+            "/var/backup"
+            "/var/lib"
+            "/root"
+          ];
+          exclude = [ "'**/.cache'" ];
+          doInit = true;
+          repo = "ssh://borgbackup@zweitwohnsitz.entropia.de/./${config.networking.hostName}.${config.networking.domain}";
+          encryption = {
+            mode = "repokey-blake2";
+            passCommand = "cat ${
+              config.sops.secrets."hosts/${config.networking.hostName}/backup_zweitwohnsitz_password".path
+            }";
+          };
+          environment = {
+            BORG_RSH = "ssh -i /etc/ssh/ssh_host_ed25519_key";
+            BORG_RELOCATED_REPO_ACCESS_IS_OK = "yes";
+          };
+          preHook = lib.mkIf config.services.postgresql.enable ''
+            ${lib.getExe pkgs.sudo} -u postgres ${pkgs.postgresql}/bin/pg_dumpall --globals-only > /var/backup/postgresql/globals
+            ${lib.getExe pkgs.sudo} -u postgres ${pkgs.postgresql}/bin/psql -t -A -c "SELECT datname FROM pg_database WHERE datname <> ALL ('{template0,template1,postgres}')" | ${pkgs.findutils}/bin/xargs -I DBNAME ${lib.getExe pkgs.sudo} -u postgres ${pkgs.postgresql}/bin/pg_dump -F directory -f /var/backup/postgresql/DBNAME DBNAME
+          '';
+          postCreate = lib.mkIf config.services.postgresql.enable ''
+            rm -r /var/backup/postgresql/*
+          '';
+          readWritePaths = lib.optionals config.services.postgresql.enable [ "/var/backup/postgresql" ];
+          compression = "auto,zlib";
+          startAt = "daily";
+        };
+
+        systemd.tmpfiles.rules = lib.mkIf config.services.postgresql.enable [
+          "d /var/backup/postgresql 0700 postgres - - -"
+        ];
+      };
 }
